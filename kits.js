6f0bdc16b721b23e7f7798af25f540f15a0c65da
unknown <1462296228@qq.com>
2019-07-04 21:05:20 +0800

    添加获取随机id方法

--- kits.js
+++ kits.js
@@ -16,6 +16,14 @@ kits.formateDate = function(){
     return y + '-' + M + '-' + D + '  ' + S +':' + F + ':' + m ;
 }
 //封装一个获取随机数的函数
-kits.primarykey = function(n,m){
+kits.randonInt = function(n,m){
     return Math.floor(Math.random() * (m - n + 1)+ n )
+}
+//封装一个可以获得id的方法
+kits.primarykey = function(){
+    let n = Date.now();
+    //防止id重复，设置一个大的值
+    let r = kits.randonInt(100000,999999);
+    //把获得的两个结果拼接起来
+    return n + '' + r;
 }
