c0679191be73ba0af820e7bacfb3fe036f18af92
unknown <1462296228@qq.com>
2019-07-04 14:37:56 +0800

    工具类添加获取事件

--- kits.js
+++ kits.js
@@ -4,3 +4,15 @@ var kits = {}
 kits.randonInt = function(n,m){
     return Math.floor(Math.random() * (m - n + 1 )+ n )
 }
+//封装一个获取时间的效果
+let kits = {}
+kits.formateDate = function(){
+    let date = new Date;
+    let y = date.getFullYear();
+    let M = date.getMonth() + 1 ;
+    let D = date.getDay();
+    let S = date.getHours();
+    let F = date.getMinutes();
+    let m = date.getSeconds();
+    return y + '-' + M + '-' + D + '  ' + S +':' + F + ':' + m ;
+}
